36eb39482ed951a769dd061c7ff5d80e74e4ae95
aviadcohz <aviadcohz@gmail.com>
2023-03-14 14:55:08 +0000

    similarity have to fix

--- run_similarity.py
+++ run_similarity.py
@@ -8,6 +8,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sbn
 import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 300
+savefig_options = dict(format="png", dpi=300, bbox_inches="tight")
+
 
 image_size = 384
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -39,30 +42,30 @@ ax.set_title("distances", size=20)
 fig.tight_layout()
 plt.savefig("distance_heatmap_alison_neg.png",format='png',dpi=150)
 
-cost , cost_pre_flip = sim.comp_accum_cost_matrix(distance_map)
-print(cost_pre_flip)
-# print(cost)
+cost = sim.comp_accum_cost_matrix(distance_map)
+
+print(cost)
 
 path, path_idx, sum_path = sim.warp_path(cost)
 print(path, path_idx, sum_path)
 
 
 
-# fig, ax = plt.subplots(figsize=(12, 8))
-# ax = sbn.heatmap(cost_matrix, annot=True, square=True, linewidths=0.1, cmap="YlGnBu", ax=ax)
-# ax.invert_yaxis()
+fig, ax = plt.subplots(figsize=(12, 8))
+ax = sbn.heatmap(cost, annot=True, square=True, linewidths=0.1, cmap="YlGnBu", ax=ax)
+ax.invert_yaxis()
 
 
-# # Get the warp path in x and y directions
-# path_x = [p[0] for p in path_idx]
-# path_y = [p[1] for p in path_idx]
+# Get the warp path in x and y directions
+path_x = [p[0] for p in path_idx]
+path_y = [p[1] for p in path_idx]
 
-# # Align the path from the center of each cell
-# path_xx = [x+0.5 for x in path_x]
-# path_yy = [y+0.5 for y in path_y]
+# Align the path from the center of each cell
+path_xx = [x+0.5 for x in path_x]
+path_yy = [y+0.5 for y in path_y]
 
-# ax.plot(path_xx, path_yy, color='blue', linewidth=3, alpha=0.2)
+ax.plot(path_xx, path_yy, color='blue', linewidth=3, alpha=0.2)
 
-# fig.savefig("alison_heatmap_path.png", **savefig_options)
+fig.savefig("alison_heatmap_path.png", **savefig_options)
 
 
--- similarity.py
+++ similarity.py
@@ -39,18 +39,30 @@ def comp_accum_cost_matrix(distances) -> np.array:
             cost[i, j] = min(cost[i-1, j],
                              cost[i,j-1],
                              cost[i-1, j-1]) + distances[i,j]
-    cost_pre_flip = cost
     cost = np.flipud(cost)
-    return cost, cost_pre_flip
+    return cost
 
 def warp_path(cost):
+    
     max_row = cost.shape[0] 
     col = cost.shape[1] - 1
     row = 0
-    path =[cost[0,col]]
+    path = [cost[0,col]]
     path_idx = [(0,col)]
+    
     while (col > 1):
+
         while (row < (max_row - 1)):
+            # When col == 0 and row below len(row)
+            if col == 0:  
+                while (row < max_row - 1):
+                    row += 1
+                    path_idx.append((row,col))
+                    path.append(cost[row,col])
+                    
+                sum_path = np.sum(path)
+                return(path, path_idx, sum_path)
+
             path.append(min(cost[row+1, col],
                              cost[row,col-1],
                              cost[row+1, col-1]))
@@ -65,5 +77,9 @@ def warp_path(cost):
                 row += 1 
                 col -= 1
             path_idx.append((row,col))
+        # When row == len(row) and col bigger than 0
+        col -= 1
+        path_idx.append((row,col))
+        path.append(cost[row,col])
     sum_path = np.sum(path)
     return(path, path_idx, sum_path)
